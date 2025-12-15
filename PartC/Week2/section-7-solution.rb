90b59a04709c1bb07ed8ed278ea6e04c9f68afae
DQ <dqisme@outlook.com>
2025-12-15 18:05:17 +0100

    pass practice problem in section 7

--- PartC/Week2/section-7-solution.rb
+++ PartC/Week2/section-7-solution.rb
@@ -7,7 +7,31 @@ class ExtendedGuessTheWordGame < GuessTheWordGame
 end
 
 class ExtendedSecretWord < SecretWord
-  ## YOUR CODE HERE
+  VALID_GUESS_PATTERN = /^\w$/
+  GUESSES = []
+
+  def self.match? (a_string, pattern_in_string)
+    /#{pattern_in_string}/i =~ a_string
+  end
+
+  def initialize word
+    self.word = word
+    self.pattern = self.word.chars.map {|character| VALID_GUESS_PATTERN =~ character ? '-' : character }.join
+  end
+
+  def valid_guess? guess
+    is_valid = !!(VALID_GUESS_PATTERN =~ guess) && !(ExtendedSecretWord.match? GUESSES.join, guess)
+    GUESSES.push guess if is_valid
+    is_valid
+  end
+
+  def guess_letter! letter
+    found = ExtendedSecretWord.match? self.word, letter
+    if found
+      self.word.chars.each_index {|index| self.pattern[index] = self.word[index] if (ExtendedSecretWord.match? self.word[index], letter)}
+    end
+    found
+  end
 end
 
 ## Change to `false` to run the original game
